6688a0cd5d8330ac4de0ff7b39c140e531ad0d91
Brian Yeh <brian@argus.gg>
2023-09-21 18:45:50 -0700

    minimal_debug

--- cardinal/component/health.go
+++
@@ -1,9 +0,0 @@
-package component
-
-import "pkg.world.dev/world-engine/cardinal/ecs"
-
-type HealthComponent struct {
-	HP int
-}
-
-var Health = ecs.NewComponentType[HealthComponent]("Health")
--- cardinal/component/player.go
+++
@@ -1,9 +0,0 @@
-package component
-
-import "pkg.world.dev/world-engine/cardinal/ecs"
-
-type PlayerComponent struct {
-	Nickname string `json:"nickname"`
-}
-
-var Player = ecs.NewComponentType[PlayerComponent]("Player")
